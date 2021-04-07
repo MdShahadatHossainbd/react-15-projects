3f2af6c828b127652f30b131a5463a1ea2acbc82
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-04-07 12:43:01 +0600

    Add files via upload

---
+++ wildfire-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+import locationIcon from '@iconify/icons-mdi/fire-alert';
+import { Icon } from '@iconify/react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered By NASA)</h1>
+        </header>
+    )
+}
+
+export default Header;
