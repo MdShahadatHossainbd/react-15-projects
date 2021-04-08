24e5bc677f047acafc6aca342e8c71836f930fba
Md Shahadat Hossain <32963275+MdShahadatHossainbd@users.noreply.github.com>
2021-04-08 22:43:34 +0600

    Add files via upload

---
+++ react-stock-website/src/App.js
@@ -0,0 +1,14 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
